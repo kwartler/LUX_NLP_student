67ca30065486eedb04c8912b065595a2ea7b4372
Giuseppe Grasso <peppegrass@gmail.com>
2021-10-15 19:13:32 +0200

    HW1 & HW2

--- lessons/oct12/HW/HW2 - WFM Bar Charts.R
+++ lessons/oct12/HW/HW2 - WFM Bar Charts.R
@@ -12,9 +12,12 @@
 setwd("~/Documents/GitHub/LUX_NLP_student/lessons/oct12/HW")
 
 ## Libraries
-library(rtweet)
+library(rtweet) 
 library(tm)
 library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(tidyr)
 
 ## Obtaing Data -- A - search term: "nobel"
 nobel       <- search_tweets(q = 'nobel', n = 1000, lang = "en")
@@ -42,7 +45,7 @@ tryTolower <- function(x){
 
 cleanCorpus<-function(corpus, customStopwords){
   corpus <- tm_map(corpus, content_transformer(qdapRegex::rm_url))
-  corpus <- tm_map(corpus, content_transformer(replace_contraction)) 
+  #corpus <- tm_map(corpus, content_transformer(replace_contraction)) 
   corpus <- tm_map(corpus, removeNumbers)
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, stripWhitespace)
@@ -52,11 +55,13 @@ cleanCorpus<-function(corpus, customStopwords){
 }
 
 ### Creating custom stop words
-stops <- c(stopwords('SMART'), 'amp')
+stops <- c(stopwords('SMART'), 'amp', 'econtwitter', 'yrs', 'years', 'year', 'yesterday')
 
 
 # Use both content() to examine 1 document content that is "cleaned"
 # Use meta() to examine 1 document's meta information
+
+## Getting data in shape to be fed to DataframeSource()
 econtwitter$doc_id  <- seq.int(nrow(econtwitter))
 econtwitter         <- econtwitter[,c(ncol(econtwitter), 1:(ncol(econtwitter)-1))]
 
@@ -71,7 +76,7 @@ nobelCorpus         <- VCorpus(DataframeSource(nobel))
 econtwitterCorpus   <- cleanCorpus(econtwitterCorpus, stops)
 nobelCorpus         <- cleanCorpus(nobelCorpus, stops)
 
-## Checking Meta Data for tweet n.10 in each of the two corpuses
+## Checking Meta Data for tweet n.100 in each of the two corpuses
 
 ### nobel
 nobelCorpus[[100]]
@@ -89,19 +94,86 @@ t(meta(econtwitterCorpus[100]))
 content(econtwitterCorpus[100])
 content(econtwitterCorpus[[100]])
 
-
 # For Data --A 
 # Create a single word frequency matrix
+
+## Creating Term Document Matrices
+nobelTdm        <- TermDocumentMatrix(nobelCorpus)
+nobelTdmM       <- as.matrix(nobelTdm)
+
+## Examining them at some random matrix slice
+nobelTdmM[1749:1751,1:4]
+
+## Word Frequency Matrix (nobel)
+nobelWordFreqMat        <- rowSums(nobelTdmM)
+nobelWordFreqMat        <- data.frame(terms = rownames(nobelTdmM), freq = nobelWordFreqMat)
+
 # Order the WFM decreasing = T
+nobelWordFreqMat <- arrange(nobelWordFreqMat, desc(freq))
+
 # Review the top 25
+head(nobelWordFreqMat, n = 25L)
+
 # Build a single bar plot with only Data --A using flipped coordinates and add an appropriate title.
+topWords      <- subset(nobelWordFreqMat, nobelWordFreqMat$freq >= 65) 
+topWords      <- topWords[order(topWords$freq, decreasing=F),]
+
+topWords$terms <- factor(topWords$terms, 
+                        levels=unique(as.character(topWords$terms))) 
+
+jpeg('barplot_nobel.jpg')
+ggplot(topWords, aes(x=terms, y=freq)) + 
+  geom_bar(stat="identity", fill='darkred') + 
+  coord_flip() + theme_gdocs() +
+  geom_text(aes(label=freq), colour="white",hjust=1.25, size=3.0) +
+  ggtitle("Frequency of top 25 terms in tweets about Nobel")
+dev.off()
 
 # For Data --B 
 # Be sure to paste/collapse both A and B
-# Merge the data set
-# Pivot the two data sets
-# Obtain the top 35 terms from both
-# Create a stacked bar chart with appropriate title
-# Create a proportional bar chart with appropriate title
+nobelWordFreqMat$terms <- NULL
+nobelWordFreqMat <- as.matrix(nobelWordFreqMat)
 
-# End
+## For the sake of variety, I obtain econtwitterWordFreqMat through a different routine where I collapse immediately and then use VectorSource()
+econtwitter             <- readRDS('econtwitter.rds')
+econtwitter             <- paste(econtwitter$text, collapse = ' ')
+econtwitterCorpus       <- VCorpus(VectorSource(econtwitter))
+econtwitterCorpus       <- cleanCorpus(econtwitterCorpus, stops)
+econtwitterWordFreqMat  <- TermDocumentMatrix(econtwitterCorpus)
+econtwitterWordFreqMat  <- as.matrix(econtwitterWordFreqMat)
+
+# Merge the data set
+bothTerms <- merge(nobelWordFreqMat, econtwitterWordFreqMat, by ='row.names')
+bothTerms <- arrange(bothTerms, desc(Row.names))
+dim(bothTerms)
+names(bothTerms) <- c('terms', 'nobel', 'econtwitter')
+head(bothTerms, n = 25L)
+
+# Pivot the two data sets
+bothTerms <- pivot_longer(bothTerms,c(nobel,econtwitter))
+
+# Obtain the top 35 terms from both
+bothTerms <- bothTerms[order(bothTerms$value, decreasing = T),]
+print(as_tibble(bothTerms), n=35)
+## get top 35 to plot
+top35 <- bothTerms[1:35,]
+top35$terms <- as.factor(top35$terms)
+
+# Create a stacked bar chart with appropriate title
+jpeg('stacked.jpg')
+ggplot(top35, aes(x=reorder(terms, -value), y = value, fill=as.factor(name) )) + 
+  geom_col() + 
+  theme_hc() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  ggtitle('word frequencies shared by nobel & econtwitter')
+dev.off()
+
+# Create a proportional bar chart with appropriate title
+jpeg('proportion.jpg')
+ggplot(top35, aes(x=reorder(terms, -value), y = value, fill=as.factor(name) )) + 
+  geom_col(position = "fill" ) + 
+  theme_hc() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  ggtitle('word frequencies shared by nobel & econtwitter')
+dev.off()
+
